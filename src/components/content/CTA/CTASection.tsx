613439970fbbb5ee7855b9d7745ac549fbc86a34
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2025-01-23 18:10:54 +0100

    Refactor CTASection layout and styling for improved visual appeal and responsiveness

--- src/components/content/CTA/CTASection.tsx
+++ src/components/content/CTA/CTASection.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function CTASection() {
   return (
-    <section className="cta_section h-screen w-full p-12">
-      <div className="w-full h-full bg-green-500"></div>
+    <section className="cta_section h-screen w-full p-4 ">
+      <div className="w-full h-full bg-green-500 rounded-md flex items-center justify-center">
+        <div>
+          <h1 className="font-saphifen text-[20vw] text-center leading-[0.8] md:text-[10vw] -rotate-6 relative z-30 ">
+            Unleash The Beast
+          </h1>
+        </div>
+      </div>
     </section>
   );
 }
